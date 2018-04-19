6c35ee49e9017b88a878db65d4dd253df9d9a592
Andrew Yuan <yuandrew@umich.edu>
2018-04-19 18:08:54 -0400

    implemented nearest search

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import secrets
 import sqlite3
+import geopy.distance
 
 # nyt_popular_key = secrets.nyt_popular_api_key
 nyt_books_key = secrets.nyt_books_api_key
@@ -16,67 +17,140 @@ yelp_client_id = secrets.yelp_client_id
 
 #Database
 DBNAME = 'data.db'
+#cache
+CACHE_FNAME = 'cache.json'
+def program_start():
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+    statement = 'DROP TABLE IF EXISTS \'Books\';'
+    cur.execute(statement)
+    conn.commit()
+    statement = 'DROP TABLE IF EXISTS \'Most_popular\';'
+    cur.execute(statement)
+    conn.commit()
+    statement = 'DROP TABLE IF EXISTS \'Yelp\';'
+    cur.execute(statement)
+    conn.commit()
+    statement = 'DROP TABLE IF EXISTS \'GMap\';'
+    cur.execute(statement)
+    conn.commit()
+
+    statement = '''
+        CREATE TABLE Books (
+            'age_group' INTEGER,
+            'author' TEXT NOT NULL,
+            'created_date' TEXT NOT NULL,
+            'description' TEXT NOT NULL,
+            'primary_isbn13' TEXT NOT NULL,
+            'title' TEXT PRIMARY KEY
+        );
+    '''
+
+    cur.execute(statement)
+    conn.commit()
+
+    statement = '''
+        CREATE TABLE Most_popular (
+            'Title' TEXT PRIMARY KEY,
+            'url' TEXT NOT NULL,
+            'published_date' TEXT NOT NULL,
+            'abstract' TEXT NOT NULL
+        );
+    '''
+
+    cur.execute(statement)
+    conn.commit()
+
+    statement = '''
+        CREATE TABLE Yelp (
+            'name' TEXT PRIMARY KEY,
+            'price' TEXT,
+            'address' TEXT,
+            'rating' REAL,
+            'review_count' INTEGER,
+            'url' TEXT,
+            'phone' TEXT,
+            FOREIGN KEY(name) REFERENCES GMaps(name)
+        );'''
+    cur.execute(statement)
+    conn.commit()
+
+    statement = '''
+        CREATE TABLE GMap (
+            'name' TEXT PRIMARY KEY,
+            'address' TEXT,
+            'distance' REAL,
+            'search_address' TEXT
+        );'''
+    cur.execute(statement)
+    conn.commit()
 conn = sqlite3.connect(DBNAME)
 cur = conn.cursor()
 
-statement = 'DROP TABLE IF EXISTS \'Books\';'
-cur.execute(statement)
-conn.commit()
-statement = 'DROP TABLE IF EXISTS \'Most_popular\';'
-cur.execute(statement)
-conn.commit()
-statement = 'DROP TABLE IF EXISTS \'Yelp\';'
-cur.execute(statement)
-conn.commit()
-statement = 'DROP TABLE IF EXISTS \'GMap\';'
-cur.execute(statement)
-conn.commit()
+# statement = 'DROP TABLE IF EXISTS \'Books\';'
+# cur.execute(statement)
+# conn.commit()
+# statement = 'DROP TABLE IF EXISTS \'Most_popular\';'
+# cur.execute(statement)
+# conn.commit()
+# statement = 'DROP TABLE IF EXISTS \'Yelp\';'
+# cur.execute(statement)
+# conn.commit()
+# statement = 'DROP TABLE IF EXISTS \'GMap\';'
+# cur.execute(statement)
+# conn.commit()
+#
+# statement = '''
+#     CREATE TABLE Books (
+#         'age_group' INTEGER,
+#         'author' TEXT NOT NULL,
+#         'created_date' TEXT NOT NULL,
+#         'description' TEXT NOT NULL,
+#         'primary_isbn13' TEXT NOT NULL,
+#         'title' TEXT PRIMARY KEY
+#     );
+# '''
+#
+# cur.execute(statement)
+# conn.commit()
+#
+# statement = '''
+#     CREATE TABLE Most_popular (
+#         'Title' TEXT PRIMARY KEY,
+#         'url' TEXT NOT NULL,
+#         'published_date' TEXT NOT NULL,
+#         'abstract' TEXT NOT NULL
+#     );
+# '''
+#
+# cur.execute(statement)
+# conn.commit()
+#
+# statement = '''
+#     CREATE TABLE Yelp (
+#         'name' TEXT PRIMARY KEY,
+#         'price' TEXT,
+#         'address' TEXT,
+#         'rating' REAL,
+#         'review_count' INTEGER,
+#         'url' TEXT,
+#         'phone' TEXT,
+#         FOREIGN KEY(name) REFERENCES GMaps(name)
+#     );'''
+# cur.execute(statement)
+# conn.commit()
+#
+# statement = '''
+#     CREATE TABLE GMap (
+#         'name' TEXT PRIMARY KEY,
+#         'address' TEXT,
+#         'distance' REAL,
+#         'search_address' TEXT
+#     );'''
+# cur.execute(statement)
+# conn.commit()
 
-statement = '''
-    CREATE TABLE Books (
-        'age_group' INTEGER,
-        'author' TEXT NOT NULL,
-        'created_date' TEXT NOT NULL,
-        'description' TEXT NOT NULL,
-        'primary_isbn13' TEXT NOT NULL,
-        'title' TEXT PRIMARY KEY
-    );
-'''
 
-cur.execute(statement)
-conn.commit()
-
-statement = '''
-    CREATE TABLE Most_popular (
-        'Title' TEXT PRIMARY KEY,
-        'url' TEXT NOT NULL,
-        'published_date' TEXT NOT NULL,
-        'abstract' TEXT NOT NULL
-    );
-'''
-
-cur.execute(statement)
-conn.commit()
-
-statement = '''
-    CREATE TABLE Yelp (
-        'name' TEXT PRIMARY KEY,
-        'price' TEXT,
-        'address' TEXT,
-        'rating' REAL,
-        'review_count' INTEGER
-        'url' TEXT NOT NULL
-    );'''
-cur.execute(statement)
-conn.commit()
-
-statement = '''CREATE TABLE GMap ('placeholder' TEXT PRIMARY KEY)'''
-cur.execute(statement)
-conn.commit()
-
-
-#cache
-CACHE_FNAME = 'cache.json'
 try:
     cache_file = open(CACHE_FNAME, 'r')
     cache_contents = cache_file.read()
@@ -146,6 +220,8 @@ def make_request_using_cache_header(baseurl, params, header):
         return CACHE_DICTION[unique_ident]
 
 def nyt_book_search(date):
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
     search_url = 'http://api.nytimes.com/svc/books/v3/lists/overview.json?'
 
     params = {'published_date': date, 'api-key': nyt_books_key}
@@ -156,60 +232,128 @@ def nyt_book_search(date):
         cur.execute(statement, insertion)
         conn.commit()
 def nyt_mostpopular_search(section, time_period):
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
     search_url = 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/' + section + '/' + str(time_period) + '.json?'
 
     params = {'api-key': nyt_books_key}
     test_search = make_request_using_cache(search_url, params)
-    print(test_search)
+    # print(test_search)
 def map_nearby_search(place):
-    lat_lon = convert_place_latlong(place)
-    lat_lon = str(lat_lon['lat']) + ',' + str(lat_lon['lng'])
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+    #calculate latitude longitude of search location
+    potential_places = get_place_id(place)
+    lat_lon_dict = convert_place_latlong(potential_places)
+    lat_lon = str(lat_lon_dict['lat']) + ',' + str(lat_lon_dict['lng'])
     search_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
     #search nearby places on google maps
     params = {'key': google_key, 'location': lat_lon, 'type': 'restaurant', 'rankby': 'distance'}
     google_search = make_request_using_cache(search_url, params)
-    # print(google_search['results'][0])
-    restaurants_nearby = []
     for result in google_search['results']:
-        restaurants_nearby.append(result['name'])
-    print(restaurants_nearby)
-
+        # Use latitude to calculate distance of restaurant from search
+        lat_lon_pair = (lat_lon_dict['lat'], lat_lon_dict['lng'])
+        rest_location = (result['geometry']['location']['lat'], result['geometry']['location']['lng'])
+        distance = calculate_distance(lat_lon_pair, rest_location)
+        # Insert data into DB
+        insert = (result['name'], result['vicinity'], distance, place)
+        statement = '''INSERT OR IGNORE INTO GMap VALUES (?, ?, ?, ?)'''
+        cur.execute(statement, insert)
+        conn.commit()
+        yelp_single_search(result['name'], place)
+    print(place)
     #and link yelp ratings to result
+    # yelp_search(place)
     #maybe create a map on flask to display on website
-
-def yelp_search(place):
+def yelp_single_search(name, place):
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
     #search yelp for restaurants in a city,
 
     search_url = 'https://api.yelp.com/v3/businesses/search?'
-    params = {'term': 'restaurants', 'location': place, 'sort_by': 'rating'}
+    params = {'term': name, 'location': place, 'sort_by': 'best_match'}
     header = {'Authorization': 'Bearer %s' % yelp_key,}
-    make_request_using_cache_header(search_url, params, header)
+    yelp_result = make_request_using_cache_header(search_url, params, header)
+    # print(name, place)
+    if len(yelp_result['businesses']) == 0:
+        statement = '''INSERT OR IGNORE INTO Yelp VALUES (?, ?, ?, ?, ?, ?, ?);'''
+        insert = (name, 'null', 'null', 'null', 'null', 'null', 'null')
+        cur.execute(statement, insert)
+        conn.commit()
+        return
+    row = yelp_result['businesses'][0]
+    address = ''
+    for item in row['location']['display_address']:
+        address += ' ' + item
+    if 'price' not in row:
+        row['price'] = ''
+    insert = (row['name'], row['price'], address, row['rating'], row['review_count'], row['url'], row['phone'])
+    # print(place, row['name'])
+    statement = '''INSERT OR IGNORE INTO Yelp VALUES (?, ?, ?, ?, ?, ?, ?);'''
+    cur.execute(statement, insert)
+    conn.commit()
+def yelp_search(place):
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
+    #search yelp for restaurants in a city,
+
+    search_url = 'https://api.yelp.com/v3/businesses/search?'
+    params = {'term': 'restaurant', 'location': place, 'sort_by': 'rating'}
+    header = {'Authorization': 'Bearer %s' % yelp_key,}
+    yelp_result = make_request_using_cache_header(search_url, params, header)
     # make_request_using_cache_header('https://api.yelp.com/oauth2/' + yelp_key, {}, header)
-    yelp_result = make_request_using_cache(search_url, params)
-    print(yelp_result)
+    # yelp_result = make_request_using_cache(search_url, params)
+    for row in yelp_result['businesses']:
+        address = ''
+        for item in row['location']['display_address']:
+            address += ' ' + item
+        insert = (row['name'], row['price'], address, row['rating'], row['review_count'], row['url'], row['phone'])
+        statement = '''INSERT OR IGNORE INTO Yelp VALUES (?, ?, ?, ?, ?, ?, ?);'''
+        cur.execute(statement, insert)
+        conn.commit()
     #maybe create a map on flask to display on website
 
-def convert_place_latlong(place):
+def get_place_id(place):
+    conn = sqlite3.connect(DBNAME)
+    cur = conn.cursor()
     #place autocomplete | Google Places API
     search_url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?'
     params = {'input': place, 'key': google_key, 'types': 'address'}
     autocomplete_search = make_request_using_cache(search_url, params)
     # print(autocomplete_search['predictions'])
-    for i in range(len(autocomplete_search['predictions'])):
-        print(str(1 + i) + ': ' + autocomplete_search['predictions'][i]['description'])
-        # print('\n\n\n\n')
-    search_index = input('Please select closest address: ')
-    place_id = autocomplete_search['predictions'][int(search_index) - 1]['place_id']
+
+    #allow user to select between different potential addresses
+    # for i in range(len(autocomplete_search['predictions'])):
+    #     print(str(1 + i) + ': ' + autocomplete_search['predictions'][i]['description'])
+    #     # print('\n\n\n\n')
+    # search_index = input('Please select closest address: ')
+    # place_id = autocomplete_search['predictions'][int(search_index) - 1]['place_id']
+
+    #add addresses to a list, first being the one to use
+    # potential_places = []
+    # for item in autocomplete_search['predictions']:
+    #     potential_places.append(item)
+    # return potential_places
+    return autocomplete_search['predictions'][0]
+
+def convert_place_latlong(place):
+    place_id = place['place_id']
+
     #Google Geocode API
     geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
     params = {'place_id': place_id, 'key': google_key}
     geo_search = make_request_using_cache(geocode_url, params)
     lat_lon = geo_search['results'][0]['geometry']['location']
 
-    print(lat_lon)
+    # print(lat_lon)
     return lat_lon
 
+def calculate_distance(coords_1, coords_2):
 
+
+    # coords_1 = (52.2296756, 21.0122287)
+    # coords_2 = (52.406374, 16.9251681)
+    return geopy.distance.vincenty(coords_1, coords_2).miles
 
 
 
@@ -220,4 +364,5 @@ def convert_place_latlong(place):
 # nyt_mostpopular_search('Arts', 1)
 # convert_place_latlong('ann arbor, MI')
 # map_nearby_search('troy, MI')
-yelp_search('troy, MI')
+# yelp_search('troy, MI')
+yelp_single_search('Sushi Ya', '4327 Whisper Way Dr.')
--- start_website.py
+++ start_website.py
@@ -1,29 +1,89 @@
 from flask import Flask, render_template, request, redirect
-
+import main
+import sqlite3
+import requests
 
 app = Flask(__name__)
-
+pub_location = ''
+class Nearby:
+    def __init__(self, init_tuple):
+        self.name = init_tuple[0]
+        self.address = init_tuple[1]
+        self.distance = format(init_tuple[2], '.2f')
+        self.price = init_tuple[3]
+        self.rating = init_tuple[4]
+        self.review_count = init_tuple[5]
+        self.url = init_tuple[6]
+        self.phone = init_tuple[7]
 @app.route('/')
 def home():
+    main.program_start()
     return render_template('home.html')
 
 
-@app.route('/select', methods = ['POST'])
+@app.route('/select', methods=['POST'])
 def signup():
-    location = request.form['location']
+    # location = request.form['location']
     search_type = request.form['search_type']
     return redirect('/' + search_type)
-#TODO: not sure how to redirect to different page based off of form selection
-# @app.route('/', methods = ['GET'])
-# def result():
-#     result = request.form
+
 @app.route('/restaurantsearch')
 def restaurant_search():
     return render_template('restaurant.html')
 
+@app.route('/restaurantsearch/confirm', methods=['POST'])
+def restaurant_search_confirm():
+    search_type = request.form['search_type']
+    location = request.form['location']
+
+    if search_type == 'nearby':
+        # potential_places = main.map_nearby_search(location)
+        main.map_nearby_search(location)
+        pub_location = location
+        return redirect('/restaurantsearch/nearby')
+    main.yelp_search(location)
+    return redirect('/restaurantsearch/city')
+
+@app.route('/restaurantsearch/nearby')
+def restaurant_search_nearby():
+    conn = sqlite3.connect(main.DBNAME)
+    cur = conn.cursor()
+    statement = '''SELECT search_address FROM Gmap'''
+    cur.execute(statement)
+    search = ''
+    for row in cur:
+        if search == '':
+            search = row[0]
+    statement = 'SELECT GMap.name, GMap.address, distance, price, rating, review_count, url, phone FROM Gmap JOIN Yelp ON Yelp.name=GMap.name'# WHERE GMap.search_address=\'' + pub_location + '\''
+    cur.execute(statement)
+    search_result = []
+    for row in cur:
+        search_result.append(Nearby(row))
+    return render_template('restaurant_nearby.html', result=search_result, place=search)
+
+@app.route('/restaurantsearch/city')
+def restaurant_search_city():
+    statement = ''' '''
+    return render_template('restaurant_city.html')
+
+
+
 @app.route('/bestsellers')
 def best_sellers():
     return render_template('bestsellers.html')
+
+@app.route('/bestsellers/confirm', methods=['POST'])
+def bs_search():
+    date = request.form['date']
+    main.nyt_book_search(date)
+    return redirect('/bestsellers/results')
+
+@app.route('/bestsellers/results')
+def best_sellers_result():
+    statement = ''' '''
+    return render_template('bestsellers_result.html')
+
+
 
 @app.route('/mostpopular')
 def most_popular():
