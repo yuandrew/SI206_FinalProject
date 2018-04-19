f5526dbbdb43036258abe9985839598642b2a4da
Andrew Yuan <yuandrew@umich.edu>
2018-04-18 23:55:30 -0400

    added paths to different pages for website

--- start_website.py
+++ start_website.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
@@ -6,6 +6,36 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/select', methods = ['POST'])
+def signup():
+    location = request.form['location']
+    search_type = request.form['search_type']
+    return redirect('/' + search_type)
+#TODO: not sure how to redirect to different page based off of form selection
+# @app.route('/', methods = ['GET'])
+# def result():
+#     result = request.form
+@app.route('/restaurantsearch')
+def restaurant_search():
+    return render_template('restaurant.html')
+
+@app.route('/bestsellers')
+def best_sellers():
+    return render_template('bestsellers.html')
+
+@app.route('/mostpopular')
+def most_popular():
+    return render_template('mostpopular.html')
+
+@app.route('/nearbysearch')
+def nearby_search():
+    return render_template('nearbysearch.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
